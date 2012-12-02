49389e54f839cd029ac2f58bd252f02c5296fc14
Aaron Faber <shnist@btinternet.com>
2012-12-02 19:00:52 +0000

    Added a basic router for filtering

---
+++ assets/script/routers/router.js
@@ -0,0 +1,24 @@
+/**
+	To do router
+*/
+
+var Router = Backbone.Router.extend({
+	routes: {
+		'*filter': 'setFilter'
+	},
+	/**
+		@function
+		@parameter {String}
+		Sets the current filter to be used and 
+		then trigger a filter event on the collection
+		which will hide / unhide to do items as specified
+	*/
+	setFilter: function () {
+		window.application.ToDoFilter = parameter.trim () || '';
+		window.application.ToDos.trigger('filter');
+	}
+});
+
+application.ToDoRouter = new Router();
+// routes the initial url during page load
+Backbone.history.start();
